4843f1a36c057693324c15f96d46d9056eb9d095
Zholobov Oleg <69785001+parlorsky@users.noreply.github.com>
2024-05-08 16:26:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import plotly.express as px
 import statsmodels.api as sm
 from sklearn.metrics import mean_squared_error
 
-df = pd.read_csv("https://drive.google.com/file/d/1FyLouCoWaUqrVlMOTb4lGCBGHs_YNfOg", sep=',')
+url = 'https://drive.google.com/file/d/1FyLouCoWaUqrVlMOTb4lGCBGHs_YNfOg/view?usp=sharing'
+pd.read_csv('https://drive.usercontent.google.com/download?id={}&export=download&authuser=0&confirm=t'.format(url.split('/')[-2]), sep = ';')
 
 st.title('')
 st.write('Выберите входные данные:')
