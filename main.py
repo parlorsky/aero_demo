6d9e6d3db713bd31499ba643b4452af344958271
Zholobov Oleg <69785001+parlorsky@users.noreply.github.com>
2024-05-08 16:29:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import statsmodels.api as sm
 from sklearn.metrics import mean_squared_error
 
 url = 'https://drive.google.com/file/d/1FyLouCoWaUqrVlMOTb4lGCBGHs_YNfOg/view?usp=sharing'
-df = pd.read_csv('https://drive.usercontent.google.com/download?id={}&export=download&authuser=0&confirm=t'.format(url.split('/')[-2]), sep = ';')
-df.columns = df.columns.str.strip()
+df = pd.read_csv('https://drive.usercontent.google.com/download?id={}&export=download&authuser=0&confirm=t'.format(url.split('/')[-2]), sep = ';', delim_whitespace=True)
+
 
 st.title('')
 st.write('Выберите входные данные:')
