158fb13b303752c5df74c37448396871439dab8a
Zholobov Oleg <69785001+parlorsky@users.noreply.github.com>
2024-05-08 16:30:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import statsmodels.api as sm
 from sklearn.metrics import mean_squared_error
 
 url = 'https://drive.google.com/file/d/1FyLouCoWaUqrVlMOTb4lGCBGHs_YNfOg/view?usp=sharing'
-df = pd.read_csv('https://drive.usercontent.google.com/download?id={}&export=download&authuser=0&confirm=t'.format(url.split('/')[-2]), sep = ';', delim_whitespace=True)
+df = pd.read_csv('https://drive.usercontent.google.com/download?id={}&export=download&authuser=0&confirm=t'.format(url.split('/')[-2]), delim_whitespace=True)
 
 
 st.title('')
